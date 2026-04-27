51ab446d5d342d2cdf13bf53abd6ba93b7195b76
Dee Adesanwo <bliteknight@gmail.com>
2026-04-27 14:59:11 -0500

    Update install_all.sh
    github raw url changed

--- install_all.sh
+++ install_all.sh
@@ -2,13 +2,13 @@ echo "##################################"
 echo "###  Installing all plugins"
 echo "##################################"
 
-wget -O ~/wire-pod/chipper/plugins/greetings.go https://raw.githubusercontent.com/bliteknight/wire-pod-plugins/main/greetings/greetings.go
+wget -O ~/wire-pod/chipper/plugins/greetings.go https://raw.githubusercontent.com/bliteknight/wire-pod-plugins/refs/heads/main/greetings/greetings.go
 /usr/local/go/bin/go build -buildmode=plugin -o ~/wire-pod/chipper/plugins/ ~/wire-pod/chipper/plugins/greetings.go 
 
-wget -O ~/wire-pod/chipper/plugins/simonsays.go https://raw.githubusercontent.com/bliteknight/wire-pod-plugins/main/simonsays/simonsays.go
+wget -O ~/wire-pod/chipper/plugins/simonsays.go https://raw.githubusercontent.com/bliteknight/wire-pod-plugins/refs/heads/main/simonsays/simonsays.go
 /usr/local/go/bin/go build -buildmode=plugin -o ~/wire-pod/chipper/plugins/ ~/wire-pod/chipper/plugins/simonsays.go 
 
-wget -O ~/wire-pod/chipper/plugins/whatdate.go https://raw.githubusercontent.com/bliteknight/wire-pod-plugins/main/whatdate/whatdate.go
+wget -O ~/wire-pod/chipper/plugins/whatdate.go https://raw.githubusercontent.com/bliteknight/wire-pod-plugins/refs/heads/main/whatdate/whatdate.go
 /usr/local/go/bin/go build -buildmode=plugin -o ~/wire-pod/chipper/plugins/ ~/wire-pod/chipper/plugins/whatdate.go 
 
 sudo systemctl restart wire-pod
